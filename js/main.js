383e994cd0a1bf1f5db762be7888de02a1f4a786
unknown <emaloa92@gamil.com>
2021-07-12 16:16:07 +0200

    Bug Fixed

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//////////////// Dshboard NavBar ///////////////////
+
+$('#bar').on('click',function(){
+  $(this).toggleClass('open');
+  $('#page-content-wrapper, #sidebar-wrapper').toggleClass('toggled')
+})
+
+/////////////////////////////
